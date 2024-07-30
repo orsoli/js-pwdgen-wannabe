f5096c3f492de0007b07cd48d652109de87fe8ca
orsoli <fljcoli@gmail.com>
2024-07-30 15:26:28 +0200

    Added analizyng phase

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
-// test if script is ok
-console.log("js ok");
+/* 
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine stampa in console nomecognomecolorepreferito21 
+*/
+
+// Analyzing phase
+/*
+1.Retrieve the element of interest from the DOM
+2.Create a constant variable (string) to save '21'
+3.Ask the user for the firstName and save the answer in a const variable
+4.Ask the user for the lastName and save the answer in a const variable
+5.Ask the user for the favoriteColor and save the answer in a const variable
+6.Print to the console this result: firstNamelastNamefavoriteColor21
+7.Bonus: Insert the result in DOM element.
+*/
